5ba15e1f4fde295794320f6448e077ed24609961
Daniel O'Flynn <danielpoflynn@gmail.com>
2024-01-10 13:08:57 +0000

    duplicate guess logic

--- client/src/components/GuessForm.js
+++ client/src/components/GuessForm.js
@@ -2,26 +2,32 @@ import { useSemantleContext } from "../hooks/useSemantleContext";
 import { useState } from "react";
 
 const GuessForm = ({ guess, setGuess }) => {
-  const { game, dispatch } = useSemantleContext();
-  const [validGuess, setValidGuess] = useState(true);
+  const { game, pastGuesses, dispatch } = useSemantleContext();
+  const [invalidMessage, setInvalidMessage] = useState(null);
 
   const handleSubmit = async (e) => {
+    // Don't reload the page
     e.preventDefault();
 
+    // Make sure guess hasn't already been made
+    if (pastGuesses.some((e) => e.simWord === guess)) {
+      return setInvalidMessage("You've already guessed this");
+    }
     const response = await fetch("/api/" + game._id + "/" + guess);
     const json = await response.json();
     if (!json.valid) {
-      setValidGuess(false);
+      setInvalidMessage("Word not found");
     }
     if (json.valid) {
-      setValidGuess(true);
+      setInvalidMessage(null);
       dispatch({ type: "NEW_GUESS", payload: json });
+      setGuess("");
     }
   };
 
   return (
     <div>
-      {validGuess && <div className="error">Error</div>}
+      {invalidMessage && <div className="error">{invalidMessage}</div>}
       <form className="guess" onSubmit={handleSubmit}>
         <input
           type="text"
--- client/src/components/PastGuesses.js
+++ client/src/components/PastGuesses.js
@@ -5,6 +5,16 @@ const PastGuesses = () => {
   const { pastGuesses, dispatch } = useSemantleContext();
   return (
     <div className="container">
+      <table>
+        <tbody>
+          <tr>
+            <th>#</th>
+            <th>Guess</th>
+            <th>Rank</th>
+            <th>Similarity</th>
+          </tr>
+        </tbody>
+      </table>
       {pastGuesses &&
         pastGuesses.map((pastGuess) => (
           <PastGuess key={pastGuess.simWord} pastGuess={pastGuess} />
