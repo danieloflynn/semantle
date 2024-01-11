27d9115ced8f2e3a5d1a5865627562f4d2b7a600
Daniel O'Flynn <danielpoflynn@gmail.com>
2024-01-11 19:48:16 +0000

    useEffect hook

--- client/src/components/Closest100.js
+++ client/src/components/Closest100.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSemantleContext } from "../hooks/useSemantleContext";
 import PastGuess from "./PastGuess";
 
@@ -7,32 +7,37 @@ const Closest100 = () => {
   const [error, setError] = useState(null);
   const [closest, setClosest] = useState(null);
   // TODO: Check if need to use useEffect for closest100
-  const getClosest100 = async () => {
-    const response = await fetch("/api/" + game._id + "/getClosest?c=101");
-    if (!response.ok) {
-      return setError("Could not get closest words");
-    }
-    const json = await response.json();
+  useEffect(() => {
+    const getClosest100 = async () => {
+      const response = await fetch("/api/" + game._id + "/getClosest?c=101");
+      if (!response.ok) {
+        return setError("Could not get closest words");
+      }
+      const json = await response.json();
 
-    setError(null);
-    return json;
-  };
-  getClosest100().then((response) => setClosest(response));
+      setError(null);
+      return json;
+    };
+    getClosest100().then((response) => setClosest(response));
+  }, [game._id]);
   return (
     <div className="container">
-      <table>
-        <tbody>
-          <tr>
-            <th>Guess</th>
-            <th>Rank</th>
-            <th>Similarity</th>
-          </tr>
-          {closest &&
-            closest.map((word) => (
-              <PastGuess key={word.simWord} pastGuess={word} />
-            ))}
-        </tbody>
-      </table>
+      {error && <p>{error}</p>}
+      {!error && (
+        <table>
+          <tbody>
+            <tr>
+              <th>Guess</th>
+              <th>Rank</th>
+              <th>Similarity</th>
+            </tr>
+            {closest &&
+              closest.map((word) => (
+                <PastGuess key={word.simWord} pastGuess={word} />
+              ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
